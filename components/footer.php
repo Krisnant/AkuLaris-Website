e1f61ecbe0e8cd0459b91b1344cbcc5b5132acf3
Krisnant <drama6399@gmail.com>
2022-12-07 21:03:40 +0700

    first commit

---
+++ components/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+
+  <div class="grid">
+
+    <div class="box-1">
+      <h3>link langsung</h3>
+      <a href="home.php">beranda</a>
+      <a href="about.php">tentang</a>
+      <a href="shop.php">Belanja</a>
+      <a href="contact.php">bantuan</a>
+    </div>
+
+    <div class="box-1">
+      <h3>extra links</h3>
+      <a href="user_login.php">login</a>
+      <a href="user_register.php">Daftar</a>
+      <a href="cart.php">keranjang</a>
+      <a href="orders.php">pesan</a>
+    </div>
+
+    <div class="box">
+      <h3>contact us</h3>
+      <a href="tel:11122233333"><i class="fab fa-whatsapp"></i> +6281359334494</a>
+      <a href="mailto:samdana015@gmail.com"><i class="fas fa-envelope"></i> samdana015@gmail.com</a>
+      <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Bululawang, Malang </a>
+    </div>
+
+  </div>
+
+  <div class="credit">Created by <span>Krisnanto</span> @<?= date('Y'); ?> | all rights reserved!</div>
+
+</footer>
